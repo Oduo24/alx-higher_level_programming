8f308b7a512c1118121014ced109acee7f4dd926
Oduo24 <geraldoduo@gmail.com>
2022-09-02 12:41:06 +0300

    if else revisited

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,71 @@
+#include "lists.h"
+/**
+ * insert_node - inserts node in a sorted list
+ * @head: head of list
+ * @number: number data
+ * Return: PTR to new node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *newNode;
+	listint_t *ptr;
+
+	if ((*head) == NULL)
+	{
+		(*head) = malloc(sizeof(listint_t));
+		if ((*head) == NULL)
+			return (NULL);
+		(*head)->n = number;
+		(*head)->next = NULL;
+		return (*head);
+	}
+
+	if (number < (*head)->n)
+	{
+		addNodeBegin(head, number);
+		return (*head);
+	}
+
+	newNode = malloc(sizeof(listint_t));
+	if (newNode == NULL)
+		return (NULL);
+	newNode->n = number;
+	newNode->next = NULL;
+
+	ptr = *head;
+
+	while (number > ptr->next->n && ptr->next->next != NULL)
+	{
+		ptr = ptr->next;
+	}
+	if (ptr->next->next == NULL)
+	{
+		add_nodeint_end(head, number);
+		return (*head);
+	}
+	newNode->next = ptr->next;
+	ptr->next = newNode;
+
+	return (newNode);
+}
+/**
+ * addNodeBegin - adding node to beginning
+ * @head: head of node
+ * @n: data of node
+ * Return: address of new element or null
+ */
+listint_t *addNodeBegin(listint_t **head, const int n)
+{
+	listint_t *tmp;
+
+	tmp = malloc(sizeof(listint_t));
+	if (tmp == NULL)
+		return (NULL);
+	tmp->n = n;
+	tmp->next = NULL;
+
+	tmp->next = *head;
+	*head = tmp;
+
+	return (*head);
+}
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    newStr = ""
+    for i in range(len(str)):
+        ASCII = ord(str[i])
+        if (ASCII >= 97 and ASCII <= 122):
+            newStr += chr(ASCII - 32)
+            continue
+        newStr += str[i]
+    print('{}'.format(newStr))

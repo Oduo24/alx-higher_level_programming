2b2853b78df900ad44032983ccbf08f64ebdfcf9
Oduo24 <geraldoduo@gmail.com>
2022-09-14 12:27:47 +0300

    ORMs

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module 2-my_filter_states"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+            argv[4])
+    )
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        print(row)
+    cursor.close()
+    db.close()

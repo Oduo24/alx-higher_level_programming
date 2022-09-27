0cb37660dfea0d0756c5765a85b7d313f3afaac8
Oduo24 <geraldoduo@gmail.com>
2022-09-27 23:44:05 +0300

    Added file 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Takes in a url and displays the http methods allowed by the server
-curl -sIL -X OPTIONS google.com | grep "Allow:" | cut -d':' -f2 | sed "s/ //"
+curl -sI -X OPTIONS google.com | grep "Allow:" | cut -d':' -f2 | sed "s/ //"

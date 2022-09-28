ae5c8af3535367a542fc6897c0a318a9d5e4bc6a
Oduo24 <geraldoduo@gmail.com>
2022-09-28 22:35:32 +0300

    Added string documentation for the module

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""0-hbtn_status module"""
+
 import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    result = response.read()
-    print("Body response:")
-    print(" - type: {}".format(type(result)))
-    print(" - content: {}".format(result))
-    print(" - utf-8 content: {}".format(result.decode('utf-8')))
+
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        result = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(result)))
+        print("\t- content: {}".format(result))
+        print("\t- utf-8 content: {}".format(result.decode('utf-8')))

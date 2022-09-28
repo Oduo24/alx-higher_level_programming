20e63c9e4efdcb9d2badff87c5b6d3a67e29392d
Oduo24 <geraldoduo@gmail.com>
2022-09-28 23:52:42 +0300

    added files from 4 to 8

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""7-error_code Module"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code:", r.status_code)
+    else:
+        print(r.text)
